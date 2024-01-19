64dbfbecbd030e624d0f208641411833d1676b7a
Дмитрий Трапезников <dima_bms@mail.ru>
2024-01-19 21:19:26 +0300

    add my theory version

---
+++ notes/tests/test_content.py
@@ -0,0 +1,37 @@
+# отдельная заметка передаётся на страницу со списком заметок в списке object_list в словаре context;
+# в список заметок одного пользователя не попадают заметки другого пользователя;
+# на страницы создания и редактирования заметки передаются формы.
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from notes.models import Note
+
+User = get_user_model()
+
+class TestContent(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Автор_1')
+        cls.note = Note.objects.create(title='Заголовок', text='Текст', author=cls.author, slug='note-slug')
+        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
+        cls.add_url = reverse('notes:add')
+        cls.author = User.objects.create(username='Комментатор')
+
+        # Запоминаем текущее время:
+      
+    def test_authorized_client_has_form_add(self):
+        # Авторизуем клиент при помощи ранее созданного пользователя.
+        self.client.force_login(self.author)
+        response = self.client.get(self.add_url)
+        self.assertIn('form', response.context)
+
+    def test_authorized_client_has_form_edit(self):
+        # Авторизуем клиент при помощи ранее созданного пользователя.
+        self.client.force_login(self.author)
+        url = reverse('notes:edit', args=(self.note.slug,))
+        response = self.client.get(url)
+        self.assertIn('form', response.context)
+
---
+++ notes/tests/test_routes.py
@@ -0,0 +1,99 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from notes.models import Note
+
+User = get_user_model()
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Автор_1')
+        cls.reader = User.objects.create(username='Автор_2')
+        cls.note = Note.objects.create(title='Заголовок', text='Текст', author=cls.author, slug='note-slug')
+
+
+    def test_pages_availability(self):
+        urls = (
+            ('notes:home', None),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK) 
+
+
+    def test_edit_note_delete_pages_availability_for_author(self):
+        with self.subTest():
+            self.client.force_login(self.author)
+            url = reverse('notes:detail', args=(self.note.slug,))
+            response = self.client.get(url)
+            print(response.status_code)
+            self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_availability_for_list_success_add_for_logged_user(self):
+        self.client.force_login(self.author)
+        for name in ('notes:list', 'notes:success', 'notes:add'):  
+            with self.subTest(user=self.author, name=name):        
+                url = reverse(name)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK) 
+
+    def test_availability_for_detail_edit_and_delete(self):
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.reader, HTTPStatus.NOT_FOUND),
+        )
+        for user, status in users_statuses:
+            self.client.force_login(user)
+            for name in ('notes:edit', 'notes:detail', 'notes:delete'):  
+                with self.subTest(user=user, name=name):        
+                    url = reverse(name, args=(self.note.slug,))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)                          
+
+
+    def test_redirect_for_anonymous_client_with_slug(self):
+        login_url = reverse('users:login')
+        for name in ('notes:detail', 'notes:edit', 'notes:delete'):
+            with self.subTest(name=name):
+                url = reverse(name, args=(self.note.slug,))
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)
+
+
+    def test_redirect_for_anonymous_client_without_slug(self):
+        login_url = reverse('users:login')
+        for name in ('notes:list', 'notes:success', 'notes:add'):
+            with self.subTest(name=name):
+                url = reverse(name)
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)                                     
+
+
+    # def test_authorized_client_has_form_edit(self):
+    #     # Авторизуем клиент при помощи ранее созданного пользователя.
+    #     self.client.force_login(self.author)
+    #     url = reverse('notes:edit', args=(self.note.slug,))
+    #     response = self.client.get(url)
+    #     self.assertIn('form', response.context)
+
+    # # 'notes:list', 'notes:success', 'notes:add',                 
+        
+    def test_authorized_client_has_form_edit(self):
+        for name in ('notes:edit'):
+            with self.subTest(name=name):
+                url = reverse(name, args=(self.note.slug,))
+                response = self.client.get(url)
+                self.assertIn('form', response.context)        
